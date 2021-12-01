4327ece9ef6af1f21a5e726fd77e87d3cf5edcb4
Raphael Hemme <hemme.raphael@gmail.com>
2021-12-01 08:22:02 +0100

    Change wording in about page slighly around and add media queries to resize banner logo under 600px screen width

--- src/app/core/about/about-page/about-page.component.ts
+++ src/app/core/about/about-page/about-page.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPageComponent implements OnInit {
 
+  public aboutP1 = 'After having worked a vew years as a researcher and lecturer at a German university, focusing on studies of science and technology and espsespecially cumputation, I felt the urge to build and practice what I was reading and writing about myself.'
+
   constructor() { }
 
   ngOnInit(): void {
