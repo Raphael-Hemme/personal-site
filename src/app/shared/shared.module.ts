6eb11b3da0855714167fc41fa3b74938bc9eac6b
Raphael Hemme <hemme.raphael@gmail.com>
2021-12-05 20:55:27 +0100

    Remove mat-card-module import from shared.module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,7 +7,6 @@ import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatListModule } from "@angular/material/list";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon"; */
-import {MatCardModule} from '@angular/material/card';
 
 
 import { P5SketchContainerComponent } from './components/p5-sketch-container/p5-sketch-container.component';
@@ -27,7 +26,6 @@ import { MatButtonModule } from '@angular/material/button';
   ],
   imports: [
     CommonModule,
-    MatCardModule,
     MatButtonModule
   ],
   exports: [
