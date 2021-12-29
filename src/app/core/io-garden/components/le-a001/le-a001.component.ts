d7c69b6822274c7d6317d37b25727b809f614b7f
Raphael Hemme <hemme.raphael@gmail.com>
2021-12-29 21:01:40 +0100

    Implement window-size dependent canvas creation to le-a001 sketch. No canvas-resizing on window-resize yet.

--- src/app/core/io-garden/components/le-a001/le-a001.component.ts
+++ src/app/core/io-garden/components/le-a001/le-a001.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import * as p5 from 'p5';
+import { WindowSizeService } from 'src/app/shared/services/window-size-service/window-size.service';
 
 
 // Based on this this sketch to generate randomly distributed dots inside a circle:
@@ -18,129 +19,113 @@ export class LeA001Component implements OnInit {
 
   public canvas: any;
 
-  constructor() {}
+  private dots: any[] = [];
+  private dots2: any[] = [];
+  private dots3: any[] = [];
+
+  private dots4: any[] = [];
+  private dots5: any[] = [];
+
+  private dots6: any[] = [];
+  private dots7: any[] = [];
+
+  private dots8: any[] = [];
+
+  private dots9: any[] = [];
+
+  public canvasWidth = 300;
+  public canvasHeight = 300;
+
+  constructor(
+    private windowSizeService: WindowSizeService
+  ) {}
 
   ngOnInit() {
+
+    const currMainContainerWidth = this.windowSizeService.getCurrentMainContainerWidth();
+    if (currMainContainerWidth >= 992) {
+      this.canvasWidth = currMainContainerWidth / 2;
+      this.canvasHeight = this.canvasWidth;
+    } else {
+      this.canvasWidth = currMainContainerWidth - 60;
+      this.canvasHeight = this.canvasWidth;
+    }
+
     const sketch = (s: any) => {
 
-      let dots: any[] = [];
-      let dots2: any[] = [];
-      let dots3: any[] = [];
-
-      let dots4: any[] = [];
-      let dots5: any[] = [];
-
-      let dots6: any[] = [];
-      let dots7: any[] = [];
-
-      let dots8: any[] = [];
-
-      let dots9: any[] = [];
-
-
-
       s.setup = () => {
-        let canvas2 = s.createCanvas(500, 500);
+        let canvas2 = s.createCanvas(this.canvasWidth, this.canvasHeight);
         canvas2.parent('le-a001-sketch-wrapper');
-
-        dots = s.makeDots(200, 100);
-        for (let el of dots) {
-          const intSubDotArr = s.makeSubDots(el.x, el.y, 10, 20)
-          dots2.push(...intSubDotArr);
-        }
-        for (let el of dots2) {
-          const intSubDotArr = s.makeSubDots(el.x, el.y, 5, 10)
-          dots3.push(...intSubDotArr);
-        }
-        dots3.forEach(el => el.size = s.random(4, 10))
-
-        dots4 = s.makeDots(50, 30);
-        dots4.forEach(el => el.size = s.random(1, 8))
-        for (let el of dots4) {
-          const intSubDotArr = s.makeSubDots(el.x, el.y, 30, 10)
-          dots5.push(...intSubDotArr);
-        }
-        dots5.forEach(el => el.size = s.random(3, 15))
-
-        dots6 = s.makeDots(300, 70);
-        for (let el of dots6) {
-          const intSubDotArr = s.makeSubDots(el.x, el.y, 10, 15)
-          dots7.push(...intSubDotArr);
-        }
-
-
-        dots8 = s.makeDots(300, 140);
-        dots8.forEach(el => el.size = s.random(1, 5))
-
-        dots9 = s.makeDots(200, 10);
-        dots9.forEach(el => el.size = s.random(1, 9))
-
       }
 
       s.draw = () => {
+        s.setupDotArrays();
         s.background(100);
         s.colorMode('RGBA')
 
-        for (var i = 0; i < dots8.length; i++){
+        for (var i = 0; i < this.dots8.length; i++){
           s.fill(163, 118, 21);
           s.noStroke();
-          s.circle(dots8[i].x, dots8[i].y, dots8[i].size);
+          s.circle(this.dots8[i].x, this.dots8[i].y, this.dots8[i].size);
         }
 
-        for (var i = 0; i < dots2.length; i++){
+        for (var i = 0; i < this.dots2.length; i++){
           s.fill(163, 118, 21);
           s.noStroke();
-          s.circle(dots2[i].x, dots2[i].y, 20);
+          s.circle(this.dots2[i].x, this.dots2[i].y, s.width / 30);
         }
-        for (var i = 0; i < dots.length; i++){
+        for (var i = 0; i < this.dots.length; i++){
           s.fill(135, 87, 14);
           s.stroke(110, 86, 36)
           s.strokeWeight(1);
-          s.circle(dots[i].x, dots[i].y, 9);
+          s.circle(this.dots[i].x, this.dots[i].y, s.width / 66.7);
         }
-        for (var i = 0; i < dots3.length; i++){
+        for (var i = 0; i < this.dots3.length; i++){
           s.strokeWeight(1);
           s.fill(196, 152, 39)
           s.stroke(133, 91, 19)
-          s.circle(dots3[i].x, dots3[i].y, dots3[i].size);
+          s.circle(this.dots3[i].x, this.dots3[i].y, this.dots3[i].size);
         }
 
         // draw dots6 and dots7
-        for (var i = 0; i < dots6.length; i++){
+        for (var i = 0; i < this.dots6.length; i++){
           s.fill(189, 145, 32);
           s.noStroke();
-          s.circle(dots6[i].x, dots6[i].y, 10);
+          s.circle(this.dots6[i].x, this.dots6[i].y, s.width / 60);
         }
-        for (var i = 0; i < dots7.length; i++){
+        for (var i = 0; i < this.dots7.length; i++){
           // noStroke();
           s.stroke(161, 125, 33, 90)
           s.fill(179, 139, 37)
-          s.circle(dots7[i].x, dots7[i].y, 9);
+          s.circle(this.dots7[i].x, this.dots7[i].y, s.width / 66.7);
         }
-
 
         // draw dots5 and dots4
-        for (var i = 0; i < dots5.length; i++){
+        for (var i = 0; i < this.dots5.length; i++){
           s.fill(133, 91, 19);
           s.noStroke();
-          s.circle(dots5[i].x, dots5[i].y, dots5[i].size);
+          s.circle(this.dots5[i].x, this.dots5[i].y, this.dots5[i].size);
         }
-        for (var i = 0; i < dots4.length; i++){
+        for (var i = 0; i < this.dots4.length; i++){
           s.fill(174, 134, 36);
           s.noStroke();
-          s.circle(dots4[i].x, dots4[i].y, dots4[i].size);
+          s.circle(this.dots4[i].x, this.dots4[i].y, this.dots4[i].size);
         }
 
         // draw dots9
-        for (var i = 0; i < dots9.length; i++){
+        for (var i = 0; i < this.dots9.length; i++){
           s.fill(107, 76, 33);
           s.noStroke();
-          s.circle(dots9[i].x, dots9[i].y, dots9[i].size);
+          s.circle(this.dots9[i].x, this.dots9[i].y, this.dots9[i].size);
         }
 
 
         s.strokeWeight(1);
         s.noFill();
+
+        console.log('dots: ', this.dots)
+
+        s.noLoop();
       }
 
       s.makeDots = (n: number, maxRadius: number) => {
@@ -150,7 +135,7 @@ export class LeA001Component implements OnInit {
           const a = s.random(0, 2 * s.PI);
 
           // https://programming.guide/random-point-within-circle.html
-          // we use square root of random for equal distribution of points        from the center
+          // we use square root of random for equal distribution of points from the center
           let r = 20 * s.sqrt(s.random(0, maxRadius));
 
           let x = s.width/2 + r * s.cos(a);
@@ -178,6 +163,50 @@ export class LeA001Component implements OnInit {
         return subDots;
       }
 
+      s.setupDotArrays = () => {
+        this.dots.splice(0, this.dots.length);
+        this.dots2.splice(0, this.dots2.length);
+        this.dots3.splice(0, this.dots3.length);
+        this.dots4.splice(0, this.dots4.length);
+        this.dots5.splice(0, this.dots5.length);
+        this.dots6.splice(0, this.dots6.length);
+        this.dots7.splice(0, this.dots7.length);
+        this.dots8.splice(0, this.dots8.length);
+        this.dots9.splice(0, this.dots9.length);
+
+        this.dots = s.makeDots(200, s.width  / 6);
+        for (let el of this.dots) {
+          const intSubDotArr = s.makeSubDots(el.x, el.y, 10, s.width / 30);
+          this.dots2.push(...intSubDotArr);
+        }
+        for (let el of this.dots2) {
+          const intSubDotArr = s.makeSubDots(el.x, el.y, 5, s.width / 60);
+          this.dots3.push(...intSubDotArr);
+        }
+        this.dots3.forEach(el => el.size = s.random(s.width / 150, s.width / 60));
+
+        this.dots4 = s.makeDots(50, s.width / 20);
+        this.dots4.forEach(el => el.size = s.random(1, s.width / 75));
+        for (let el of this.dots4) {
+          const intSubDotArr = s.makeSubDots(el.x, el.y, 30, s.width / 60);
+          this.dots5.push(...intSubDotArr);
+        }
+        this.dots5.forEach(el => el.size = s.random(s.width / 200, s.width / 40));
+
+        this.dots6 = s.makeDots(300, s.width / 8.6);
+        for (let el of this.dots6) {
+          const intSubDotArr = s.makeSubDots(el.x, el.y, 10, s.width / 40);
+          this.dots7.push(...intSubDotArr);
+        }
+
+
+        this.dots8 = s.makeDots(300, s.width / 4.3);
+        this.dots8.forEach(el => el.size = s.random(1, 5));
+
+        this.dots9 = s.makeDots(200, s.width / 60);
+        this.dots9.forEach(el => el.size = s.random(1, 9));
+      }
+
     };
 
     this.canvas = new p5(sketch);
@@ -185,6 +214,11 @@ export class LeA001Component implements OnInit {
 
   ngOnDestroy(): void {
     this.canvas.remove();
+  }
+
+  public handleReloadBtn() {
+    this.canvas.clear();
+    this.canvas.redraw(1);
   }
 
 }
