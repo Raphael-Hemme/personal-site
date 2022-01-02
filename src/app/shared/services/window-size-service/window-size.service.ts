b31ae27052b7dfcf7c5e16354ed28f9e649b6f7b
Raphael Hemme <hemme.raphael@gmail.com>
2022-01-02 12:20:28 +0100

    Fix canvas-size calculation to stick to container on 100% factor be subtracting container padding etc.. Works for now but is overly specific. Refactor later.

--- src/app/shared/services/window-size-service/window-size.service.ts
+++ src/app/shared/services/window-size-service/window-size.service.ts
@@ -47,12 +47,23 @@ export class WindowSizeService {
       let canvWidth = 0;
       let canvHeight = 0;
 
+      console.log(this.currMainContainerWidth)
+
       if (window.innerWidth >= 992) {
         // Subtract 10px from actual width * percent to handle padding-right of 10px and have a fluid canvas
         canvWidth = this.currMainContainerWidth / 100 * canvasSizeParamObj.wPercentL - 10;
+        // canvWidth = this.currMainContainerWidth / 100 * canvasSizeParamObj.wPercentL;
+        canvHeight = this.currMainContainerHeight / 100 * canvasSizeParamObj.hPercentL;
+      } else if (window.innerWidth > 720 && window.innerWidth < 992) {
+        // Subtract 10px from actual width * percent to handle padding-right of 10px and have a fluid canvas
+        console.log(this.currMainContainerWidth)
+        canvWidth = this.currMainContainerWidth / 100 * canvasSizeParamObj.wPercentL - 35;
+        // canvWidth = this.currMainContainerWidth / 100 * canvasSizeParamObj.wPercentL;
         canvHeight = this.currMainContainerHeight / 100 * canvasSizeParamObj.hPercentL;
       } else {
-        canvWidth = this.currMainContainerWidth / 100 * canvasSizeParamObj.wPercentS;
+        // Subtract 20px from actual width * percent to handle padding on both sides of 10px and have a fluid canvas
+        // canvWidth = this.currMainContainerWidth / 100 * canvasSizeParamObj.wPercentS - 20;
+        canvWidth = this.currMainContainerWidth / 100 * canvasSizeParamObj.wPercentS - 20;
         canvHeight = this.currMainContainerHeight / 100 * canvasSizeParamObj.hPercentS;
       }
 
