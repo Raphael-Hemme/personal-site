6e255dfe120c65e2bc08468f15a7f47785c890d1
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2022-01-30 17:18:28 +0100

    Change io-garden explanation and abstract. Add background image and overlay to io-garden abstract container and change text layout.

--- src/app/core/io-garden/io-garden-page/io-garden-page.component.ts
+++ src/app/core/io-garden/io-garden-page/io-garden-page.component.ts
@@ -10,13 +10,13 @@ export class IoGardenPageComponent implements OnInit {
 
   public ioWelcomeMain = `Welcome to Io-Garden!`;
 
-  public ioWelcomeSub = `Leave the void behind and enter my humble glasshouse.\n Take a look around, have a chat but don't throw rocks.`;
+  public ioAbstract = `Leave the void behind and enter my humble glasshouse.\n Take a look around, have a chat but don't throw rocks.\n\n Io-Garden is a space to cultivate ideas and concepts - to experiment with algorithms and impressions.\n
+    Freshly planted seedlings can evolve and flourish, as can the vines and trees sprawling under the special greenhouse construction that shields this digital garden from Io's atmosphere.`;
+/* Io-Garden is my extraterrestrial research lab and open studio. */
 
-  public ioAbstract = `Io-Garden is a space for growing and experimenting with ideas and concepts, algorithms and impressions.\n
-    Freshly planted seeds and seedlings, as well as shrubs, trees and vines can evolve and flourish under the special greenhouse construction that shields this digital garden from Io's atmosphere. Io-Garden is my extraterrestrial research lab and open studio.`;
 
   public ioNameExplainer = `Why build a laboratory greenhouse on Io?\n
-    Well, born on (or in?) Europa, I felt, Io was underrated and a welcome change. Property was affordable and, having figured out a way to utilize the strong electro magnetic interference, I was able to significantly lower my energy costs.\n
+    Well, born on (or in?) Europa, I felt, Io was underrated and a welcome change of air. Property was affordable and, having figured out a way to utilize the strong electro magnetic interference, I was able to significantly lower my energy costs.\n
     Ok, ok... I admit this is not entirely true.\n There is, of course, no property law on Io. And that makes it even more attractive doesn't it?\n
     Also, this whole complex was intended to be a digital garden all along. But 'Digital Garden' is quite a long name to stuff into a menu bar. So, I decided to shorten it to '1/0-garden' since 'one / zero' is a nice shorthand for 'digital'. The problem with '1/0-garden', however, is that you should not use numbers to start a JavaScript class name (which was needed for the class based Angular component). That led me to 'i/o' which still resembled '1/0' and also 'input / output', which was a less welcome but bearable connotation. But, as you will have guessed, there was still a problem left: '/' is also not allowed in class names. Hence, that had to go as well... leaving me with 'io-garden. I liked the sound of the name and pictures of a lush greenhouse on Io were already taking shape as a kind of 'mind palace'.\n
     If you struggle with the pronounciation of 'Io-Garden', or are simply more used to one of the languages of Europa or somewhere else in space, you can, of course, also call it for example 'Ionische Gärten' or tell me what this place is called in your mother tongue.`;
