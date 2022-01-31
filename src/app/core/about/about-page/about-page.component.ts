f603ed69afb297fda9a177b25349e9c688a65a4e
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2022-01-31 21:34:32 +0100

    Edit text on about page

--- src/app/core/about/about-page/about-page.component.ts
+++ src/app/core/about/about-page/about-page.component.ts
@@ -9,7 +9,8 @@ export class AboutPageComponent implements OnInit {
 
   public aboutP1 = `After having worked a few years as a researcher and lecturer at a German university, 
     focusing on anthropological studies of science and technology in the emerging field of 'affective cumputing', 
-    I felt the urge to build and practice what I was reading and writing about myself.`
+    I felt the urge to build and practice myself what I was reading and writing about. From an anthropological perspective,
+    I was in the process of 'going native', although not strictly in the field of 'affective computing'.`
   public aboutP2 = `The decision to change careers and take up coding as my daily routine and profession was helped by
     the fact that I was drawn to practices with a more immediate, more tangible or at least more visible outcome. 
     While I enjoy the analytical engagement with theory and empirical data, and the shifts in your worldview
@@ -19,10 +20,14 @@ export class AboutPageComponent implements OnInit {
     'Gestaltung' or design. I like that programming allows me to do exactly that and that it frequently triggers interesting
     philosophical reflections and discussions, keeps me curios and constantly in a mode of solving puzzles.`;
   public aboutP3 = `My appreciation for this meditative immersion into a practice of searching for the right shape and form
-  goes back to an early engagement with graffiti and street art, accompanied me through my studies of anthropology 
-  - albeit much less actively - and currently manifests itself in my daily work as a web developer and the frequent trips
-  into the domain of typography and logo design. Given this strong inclination to the visual, I think it is no surprise
-  that I focus on frontend development since I feel it comes natural to me.`;
+  goes back to an early engagement with graffiti and street art, it accompanied me through my studies of anthropology 
+  and the countless hours of refining and sharpening an argument or the general composition of an essay. 
+  Currently I enjoy this flow state in my daily work as a web developer and in my frequent trips into the domain of
+  typography and logo design. 
+  Given a disposition to a more visual style of thinking than most accademic work in the humanities and social sciences faccilitate, 
+  I think it is no surprise that I now focus mainly on frontend development since I feel it comes natural to me. And yet, I am still 
+  fascinated both by the socio-cultural context and momentum of computational technology and by the technological fabric and the 
+   interwoven with user interfaces often more strongly associated with the backend .`;
 
   constructor() { }
 
