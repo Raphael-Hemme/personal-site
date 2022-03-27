8eb24f6e213e3316c8c95c669f464d6fb06c5c37
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2022-03-27 13:04:22 +0200

    Change h4 styling

--- src/app/core/io-garden/io-garden-page/io-garden-page.component.ts
+++ src/app/core/io-garden/io-garden-page/io-garden-page.component.ts
@@ -14,7 +14,7 @@ export class IoGardenPageComponent implements OnInit {
 /* Io-Garden is my extraterrestrial research lab and open studio. */
 
 
-  public ioNameExplainer = `Io-Garden is not about input and output. Io-Garden is an experimental space located on Io.\n\n "What?", you might ask and wonder: "Why would you build a greenhouse on Io?"\n
+  public ioNameExplainer = `Io-Garden is not about input and output. Io-Garden is an experimental space located on Io.\n\n "What?", you might ask and wonder: "Why would you build a digital garden on Io?"\n
     I'm glad you asked!\n Born on (or in?) Europa, I felt, Io was underrated and a welcome change of air. Property was affordable and, having figured out a way to utilize the strong electro magnetic interference, I was able to significantly lower my energy costs.\n
     Ok, ok... I admit this is not entirely true.\n There is, of course, no property law on Io. And that makes it even more attractive doesn't it?\n
     Also, this whole complex was intended to be a digital garden all along. But 'Digital Garden' is quite a long name to stuff into a menu bar. So, I decided to shorten it to '1/0-garden' since 'one / zero' is a nice shorthand for 'digital'. The problem with '1/0-garden', however, is that you should not use numbers to start a JavaScript class name (which was needed for the class based Angular component). That led me to 'i/o' which still resembled '1/0'. Since I'm concerned about the reductionist approach and effect of the information processing model, I don't use 'i/o' to signify 'input / output' as it usually does.\n
