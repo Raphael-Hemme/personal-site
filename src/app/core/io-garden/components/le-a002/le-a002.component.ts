d0b42e6fda2d752b96916f49be2b654496a2c9c5
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2022-04-03 18:54:19 +0200

    Make le-a002 work generally WIP: find way to use cell.changeSaturation method

--- src/app/core/io-garden/components/le-a002/le-a002.component.ts
+++ src/app/core/io-garden/components/le-a002/le-a002.component.ts
@@ -71,20 +71,6 @@ export class LeA002Component implements OnInit, OnDestroy {
 
   public canvas: any;
 
-  private dots: any[] = [];
-  private dots2: any[] = [];
-  private dots3: any[] = [];
-
-  private dots4: any[] = [];
-  private dots5: any[] = [];
-
-  private dots6: any[] = [];
-  private dots7: any[] = [];
-
-  private dots8: any[] = [];
-
-  private dots9: any[] = [];
-
   public canvWidth = 300;
   public canvHeight = 300;
 
@@ -112,159 +98,185 @@ export class LeA002Component implements OnInit, OnDestroy {
 
     const sketch = (s: any) => {
 
+      let cells: any[] = [];
+      let subCells: any[] = [];
+
+      let canvasCenter = {
+        x: 0,
+        y: 0
+      }
+      const distLimit = 400
+
       s.setup = () => {
         let canvas2 = s.createCanvas(this.canvWidth, this.canvHeight);
         canvas2.parent('le-a002-sketch-wrapper');
+        canvasCenter.x = s.width / 2;
+        canvasCenter.y = s.height / 2;
+        s.frameRate(20);
       }
 
       s.draw = () => {
-        s.setupDotArrays();
         s.background(100);
-        s.colorMode('RGBA')
+        
 
-        for (var i = 0; i < this.dots8.length; i++){
-          s.fill(163, 118, 21);
-          s.noStroke();
-          s.circle(this.dots8[i].x, this.dots8[i].y, this.dots8[i].size);
+        if (s.frameCount % 30 === 0) {
+          console.log('cells length: ', cells.length)
+          console.log('subCells length: ', subCells.length)
         }
 
-        for (var i = 0; i < this.dots2.length; i++){
-          s.fill(163, 118, 21);
-          s.noStroke();
-          s.circle(this.dots2[i].x, this.dots2[i].y, s.width / 30);
+        const currGenCount = s.round(s.random(3, 25))
+        const minSize = 2
+        const maxSize = 15
+        const maxDist = s.round(s.random(10, distLimit))
+        const mainCellMaxAge = s.round(s.random(300, 500));
+        s.generateCells(currGenCount, minSize, maxSize, canvasCenter, maxDist, cells, mainCellMaxAge);
+      
+        s.drawCells(cells);
+        s.ageCells(cells);
+        s.multiplyCells(cells)
+        s.killCells(cells);
+        if (subCells.length > 0) {
+          s.drawCells(subCells);
+          s.ageCells(subCells);
+          s.setRandomCellsToHaveBloomShape(subCells)
+          s.killCells(subCells);
         }
-        for (var i = 0; i < this.dots.length; i++){
-          s.fill(135, 87, 14);
-          s.stroke(110, 86, 36)
-          s.strokeWeight(1);
-          s.circle(this.dots[i].x, this.dots[i].y, s.width / 66.7);
-        }
-        for (var i = 0; i < this.dots3.length; i++){
-          s.strokeWeight(1);
-          s.fill(196, 152, 39)
-          s.stroke(133, 91, 19)
-          s.circle(this.dots3[i].x, this.dots3[i].y, this.dots3[i].size);
-        }
-
-        // draw dots6 and dots7
-        for (var i = 0; i < this.dots6.length; i++){
-          s.fill(189, 145, 32);
-          s.noStroke();
-          s.circle(this.dots6[i].x, this.dots6[i].y, s.width / 60);
-        }
-        for (var i = 0; i < this.dots7.length; i++){
-          // noStroke();
-          s.stroke(161, 125, 33, 90)
-          s.fill(179, 139, 37)
-          s.circle(this.dots7[i].x, this.dots7[i].y, s.width / 66.7);
-        }
-
-        // draw dots5 and dots4
-        for (var i = 0; i < this.dots5.length; i++){
-          s.fill(133, 91, 19);
-          s.noStroke();
-          s.circle(this.dots5[i].x, this.dots5[i].y, this.dots5[i].size);
-        }
-        for (var i = 0; i < this.dots4.length; i++){
-          s.fill(174, 134, 36);
-          s.noStroke();
-          s.circle(this.dots4[i].x, this.dots4[i].y, this.dots4[i].size);
-        }
-
-        // draw dots9
-        for (var i = 0; i < this.dots9.length; i++){
-          s.fill(107, 76, 33);
-          s.noStroke();
-          s.circle(this.dots9[i].x, this.dots9[i].y, this.dots9[i].size);
-        }
-
-
-        s.strokeWeight(1);
-        s.noFill();
-
-        // console.log('dots: ', this.dots)
-
-        s.noLoop();
+        
       }
 
-      s.makeDots = (n: number, maxRadius: number) => {
-        const internalDotArr = []
-      //   choose random radius and angle from the center
-        for (var i = 0; i < n; i++){
-          const a = s.random(0, 2 * s.PI);
-
-          // https://programming.guide/random-point-within-circle.html
-          // we use square root of random for equal distribution of points from the center
-          let r = 20 * s.sqrt(s.random(0, maxRadius));
-
-          let x = s.width/2 + r * s.cos(a);
-          let y = s.height / 2 + r * s.sin(a);
-          var newDot = {x: x, y: y};
-          internalDotArr.push(newDot);
+      s.drawCells = (targetCellArr: any[]) => {
+        for(let cell of targetCellArr){
+          let oldX = cell.x;
+          let oldY = cell.y;
+      
+          cell.x += cell.xSpeed;
+          cell.y += cell.ySpeed;
+      
+          cell.traveledDist += s.dist(oldX, oldY, cell.x, cell.y);
+       
+          if (cell.traveledDist > cell.maxDist){
+            cell.xSpeed = 0;
+            cell.ySpeed = 0;
+          }
+      
+          let c = s.color(`hsla(${cell.hsla.h}, ${cell.hsla.s}%, ${cell.hsla.l}%, ${cell.hsla.a})`);
+          if (!cell.blooming) {
+            s.noStroke();
+            s.fill(c);
+          } else {
+            s.noFill()
+            s.strokeWeight(2);
+            s.stroke(c);
+          }
+          s.ellipse(cell.x, cell.y, cell.size, cell.size);
         }
-        return internalDotArr;
       }
 
-      s.makeSubDots = (subDotCenterX: number, subDotCenterY: number, maxAmount: number, rRange: number) => {
-        //   choose random radius and angle from the center
-        const amount = s.random(0, maxAmount);
-        const subDots = [];
-        for (var i = 0; i < amount; i++){
-
-          const a = s.random(0, 2 * s.PI);
-          const r = s.random(5, rRange);
-
-          let x = subDotCenterX + r * s.cos(a);
-          let y = subDotCenterY + r * s.sin(a);
-          var newDot = {x: x, y: y};
-          subDots.push(newDot);
+      s.generateCells = (
+        amount: number,
+        minSize: number,
+        maxSize: number,
+        center: any,
+        maxDist: number,
+        targetCellArr: any,
+        maxAge: number) => {
+        for(let i = 0; i < amount; i++) {
+      
+          const x = center.x // width / 2; 
+          const y = center.y // height / 2; 
+      
+          const xSpeed = s.random(-5, 5);
+          const ySpeed = s.random(-5, 5);
+      
+      /*     const xSpeed = random(-1, 1);
+          const ySpeed = random(-1, 1); */
+      
+          const maxTravelDist = s.round(s.random(maxDist / 2, maxDist))
+          const traveledDist = 0;
+          const size = s.random(minSize, maxSize);
+      
+          const hsla = {
+            h: 40,
+            s: 90, // random(30, 90),
+            l: 40,
+            a: 1.0
+          };
+      
+          targetCellArr.push(new Cell(
+            x,
+            y,
+            size,
+            xSpeed,
+            ySpeed,
+            traveledDist,
+            maxTravelDist,
+            maxDist,
+            hsla,
+            maxAge
+          ))
         }
-        return subDots;
       }
 
-      s.setupDotArrays = () => {
-        this.dots.splice(0, this.dots.length);
-        this.dots2.splice(0, this.dots2.length);
-        this.dots3.splice(0, this.dots3.length);
-        this.dots4.splice(0, this.dots4.length);
-        this.dots5.splice(0, this.dots5.length);
-        this.dots6.splice(0, this.dots6.length);
-        this.dots7.splice(0, this.dots7.length);
-        this.dots8.splice(0, this.dots8.length);
-        this.dots9.splice(0, this.dots9.length);
-
-        this.dots = s.makeDots(200, s.width  / 6);
-        for (let el of this.dots) {
-          const intSubDotArr = s.makeSubDots(el.x, el.y, 10, s.width / 30);
-          this.dots2.push(...intSubDotArr);
-        }
-        for (let el of this.dots2) {
-          const intSubDotArr = s.makeSubDots(el.x, el.y, 5, s.width / 60);
-          this.dots3.push(...intSubDotArr);
-        }
-        this.dots3.forEach(el => el.size = s.random(s.width / 150, s.width / 60));
-
-        this.dots4 = s.makeDots(50, s.width / 20);
-        this.dots4.forEach(el => el.size = s.random(1, s.width / 75));
-        for (let el of this.dots4) {
-          const intSubDotArr = s.makeSubDots(el.x, el.y, 30, s.width / 60);
-          this.dots5.push(...intSubDotArr);
-        }
-        this.dots5.forEach(el => el.size = s.random(s.width / 200, s.width / 40));
-
-        this.dots6 = s.makeDots(300, s.width / 8.6);
-        for (let el of this.dots6) {
-          const intSubDotArr = s.makeSubDots(el.x, el.y, 10, s.width / 40);
-          this.dots7.push(...intSubDotArr);
-        }
-
-
-        this.dots8 = s.makeDots(300, s.width / 4.3);
-        this.dots8.forEach(el => el.size = s.random(1, 5));
-
-        this.dots9 = s.makeDots(200, s.width / 60);
-        this.dots9.forEach(el => el.size = s.random(1, 9));
+    // Not for generating new cells (rename later) just visual
+    s.setRandomCellsToHaveBloomShape = (targetCellArr: any) =>  {
+      for (let cell of targetCellArr) {
+        if (cell.xSpeed === 0 && cell.canBloom) {
+          cell.bloom();
+        } 
       }
+    }
+
+    s.killCells = (targetCellArr: any) => {
+      const survivingCellArr = targetCellArr.filter((cell: any) => cell.age < cell.maxAge);
+      targetCellArr.splice(0, targetCellArr.length);
+      for (let el of survivingCellArr) {
+        targetCellArr.push(el);
+      }
+    }
+
+    s.ageCells = (targetCellArr: any) => {
+      for (let cell of targetCellArr) {
+        cell.incrementAge();
+        // cell.changeSaturation();
+        if (cell.age > cell.maxAge / 2) {
+          cell.fade();
+        }
+      }
+    }
+
+    s.multiplyCells = (targetCellArr: any) => {
+      for (let cell of targetCellArr) {
+        if (cell.xSpeed === 0 
+          && cell.age > 170 
+          && cell.canReproduce 
+          && subCells.length < 6000) {
+            cell.bloom()
+            cell.canReproduce = false;
+            const currGenCount = s.round(s.random(20, 40))
+            const currMinSize = 2
+            const currMaxSize = 15
+            const currMaxDist = s.round(s.random(10, 50))
+            const currCenter = {
+              x: cell.x,
+              y: cell.y
+          }
+          const descendentMaxAge = s.round(s.random(100, 200))
+          s.generateCells(
+            currGenCount,
+            currMinSize,
+            currMaxSize,
+            currCenter,
+            currMaxDist,
+            subCells,
+            descendentMaxAge
+          );
+        }
+      }
+      
+    }
+    
+    
+
 
     };
 
