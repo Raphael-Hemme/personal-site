e7dc703f81f2aafcaa05c0dbbdede7607273502d
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2022-08-14 13:37:06 +0200

    Hand copy and adapt coding trains OOP fractal tree example branch file from JS to TS

---
+++ src/app/core/io-garden/components/le-a003/branch.ts
@@ -0,0 +1,35 @@
+import p5 from "p5";
+
+
+class Branch {
+    begin
+    end
+    finished
+    s
+
+    constructor(begin: any, end: any, sketch: p5) {
+        this.begin = begin;
+        this.end = end;
+        this.finished = false;
+        this.s = sketch
+    }
+
+    show() {
+        this.s.stroke(2500);
+        this.s.line(this.begin.x, this.begin.y, this.end.x, this.end.y);
+    }
+
+    branch(first: boolean) {
+        let dir = p5.Vector.sub(this.end, this.begin);
+        if (first) {
+            dir.rotate(this.s.PI / 6);
+            dir.mult(0.67);
+        } else {
+            dir.rotate(-this.s.PI / 4);
+            dir.mult(0.67);
+        }
+        let newEnd = p5.Vector.add(this.end, dir);
+        let result = new Branch(this.end, newEnd, this.s);
+        return result; 
+    }  
+}
