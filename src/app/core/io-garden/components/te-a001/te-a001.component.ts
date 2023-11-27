a355dabfc42f2c4173b4b75fa3a0d557a36b4f1e
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-11-27 22:21:34 +0100

    refactor: big refactor / rewrite of TeA001Component - first working version, more work coming

--- src/app/core/io-garden/components/te-a001/te-a001.component.ts
+++ src/app/core/io-garden/components/te-a001/te-a001.component.ts
@@ -8,29 +8,6 @@ interface LineObj {
   end: p5.Vector;
 }
 
-class DotTriplet {
-  constructor(x1: number, y1: number) {
-    this.firstDot.x = x1;
-    this.firstDot.y = y1;
-    this.secondDot.x = x1 + 10;
-    this.secondDot.y = y1;
-    this.thirdDot.x = x1;
-    this.thirdDot.y = y1 + 10;
-  }
-  firstDot = {
-    x: 0,
-    y: 0,
-  };
-  secondDot = {
-    x: 0,
-    y: 0,
-  };
-  thirdDot = {
-    x: 0,
-    y: 0,
-  };
-}
-
 @Component({
   selector: 'app-te-a001',
   templateUrl: './te-a001.component.html',
@@ -40,88 +17,13 @@ export class TeA001Component implements OnInit, OnDestroy {
   public canvWidth = 500;
   public canvHeight = 500;
 
+  private charWidth = 100;
+
   public inputText = '';
 
   public canvas: any;
 
-  private chars: { [key: string]: {} } = {
-    a: {},
-    b: {},
-    c: {},
-    d: {},
-    e: {},
-    f: {},
-    g: {},
-    h: {},
-    i: {},
-    j: {},
-    k: {},
-    l: {},
-    m: {},
-    n: {},
-    o: {},
-    p: {},
-    q: {},
-    r: {},
-    s: {},
-    t: {},
-    u: {},
-    v: {},
-    w: {},
-    x: {},
-    y: {},
-    z: {},
-    ß: {},
-    ä: {},
-    á: {},
-    é: {},
-    è: {},
-    ì: {},
-    ö: {},
-    ü: {},
-    '0': {},
-    '1': {},
-    '2': {},
-    '3': {},
-    '4': {},
-    '5': {},
-    '6': {},
-    '7': {},
-    '8': {},
-    '9': {},
-    space: 'space',
-    return: 'return',
-    '!': {},
-    '@': {},
-    '#': {},
-    $: {},
-    '%': {},
-    '^': {},
-    '&': {},
-    '*': {},
-    '(': {},
-    ')': {},
-    '-': {},
-    _: {},
-    '+': {},
-    '=': {},
-    '[': {},
-    ']': {},
-    '{': {},
-    '}': {},
-    ',': {},
-    '.': {},
-    '?': {},
-    '/': {},
-    '<': {},
-    '>': {},
-    '~': {},
-    '`': {},
-    '"': {},
-    "'": {},
-    '|': {},
-    '\\': {},
-  };
+  private chars: Map<number, any> = new Map();
 
   private ioStringArr: any[] = [];
 
@@ -141,8 +43,7 @@ export class TeA001Component implements OnInit, OnDestroy {
       hPercentL: 50,
     };
 
-    const canvSizeObj =
-      this.windowSizeService.calculateCanvasSize(canvasConfig);
+    const canvSizeObj = this.windowSizeService.calculateCanvasSize(canvasConfig);
     this.canvWidth = canvSizeObj.w;
     this.canvHeight = canvSizeObj.h;
 
@@ -157,10 +58,8 @@ export class TeA001Component implements OnInit, OnDestroy {
         let canvas2 = s.createCanvas(this.canvWidth, this.canvHeight);
         canvas2.parent('te-a001-sketch-wrapper');
 
-        this.generateAlphabet(s);
-
         s.background(50);
-        s.frameRate(20);
+        s.frameRate(10);
 
         s.fill(237, 34, 93);
         // s.noStroke();
@@ -170,18 +69,7 @@ export class TeA001Component implements OnInit, OnDestroy {
         s.frameCount === 1 ?? s.translate(0, 10);
 
         s.background(50);
-        this.ioStringArr.splice(0, this.ioStringArr.length);
-        this.ioStringArr = this.translateInputStringIntoIoArr(
-          s,
-          this.inputText
-        );
-
-        this.drawString(s, this.ioStringArr);
-        // const {start, end} = this.generateDiagonalLineStartAndEnd(s);
-        // s.stroke(255);
-        // s.strokeWeight(2);
-        // s.line(start[0], start[1], end[0], end[1]);
-        // s.noLoop();
+        this.drawString(s, this.inputText);
       };
     };
 
@@ -193,174 +81,46 @@ export class TeA001Component implements OnInit, OnDestroy {
     this.subscriptions.unsubscribe();
   }
 
-  private drawChar(s: p5, charPointArr: any): any {
-    // new drawing function: charPointArr is not renamed to be less confusing if old function
-    // is restored. It is an object however.
-    for (let hbar of charPointArr.horizontalBarArr) {
-      s.rect(hbar.x, hbar.y, hbar.w, hbar.h);
-    }
-    for (let vbar of charPointArr.verticalBarArr) {
-      s.rect(vbar.x, vbar.y, vbar.w, vbar.h);
-    }
-    for (let dot in charPointArr.dotTriplet) {
-      // console.log(dot)
-      // circle(charPointArr[dot].x, charPointArr[dot].y, charPointArr[dot].w, charPointArr[dot].h)
-    }
-    s.circle(
-      charPointArr.dotTriplet.firstDot.x,
-      charPointArr.dotTriplet.firstDot.y,
-      5
-    );
-    s.circle(
-      charPointArr.dotTriplet.secondDot.x,
-      charPointArr.dotTriplet.secondDot.y,
-      5
-    );
-    s.circle(
-      charPointArr.dotTriplet.thirdDot.x,
-      charPointArr.dotTriplet.thirdDot.y,
-      5
-    );
-  }
-
-  private drawString(s: p5, stringArr: any[]) {
-    if (stringArr.includes('return')) {
-      const preSubStringArr = stringArr.map((el) => {
-        // maybe find a better solution for the substitution characters
-        // in order to not block them. Maybe Reg-Ex...
-        if (el === 'return') {
-          return '~';
-        } else if (el === 'space') {
-          return '_';
-        } else {
-          return el;
-        }
-      });
-      const subStringArr = preSubStringArr.join('').split('~');
-      for (let i = 0; i < subStringArr.length; i++) {
-        const currYOffset = i > 0 ? 150 : 0;
-        const currXOffset = i > 0 ? -1 * (subStringArr[i - 1].length * 100) : 0;
-        s.translate(currXOffset, currYOffset);
-        for (let char of subStringArr[i]) {
-          if (char === '_') {
-            s.translate(100, 0);
-          } else {
-            s.translate(100, 0);
-            this.drawChar(s, this.chars[char]);
-          }
+  private drawString(s: p5, inputStr: string) {
+    // split inputStr into array of strings for each line.
+    const gap = 20
+    const lineHeight = 120;
+    const charWidth = 100;
+    let currLine  = 1;
+    const lineStrArr = inputStr.split('\n');
+    for (const [i, line] of lineStrArr.entries()) {
+      const currYOffset = currLine < 2 ? gap : (currLine - 1) * lineHeight + gap;
+      for (const [j, char] of line.split('').entries()) {
+        const currXOffset = j < 1 ? gap : j * charWidth + gap;;
+        const currCharCode = char.toLowerCase().charCodeAt(0);
+        if (this.chars.has(currCharCode)) {
+          s.push();
+            s.translate(currXOffset, currYOffset);
+            this.drawChar(s, this.chars.get(currCharCode));
+          s.pop();
         }
       }
-    } else {
-      for (let char of stringArr) {
-        if (char === 'space') {
-          s.translate(100, 0);
-        } else {
-          s.translate(100, 0);
-          this.drawChar(s, this.chars[char]);
-        }
+      currLine++;
+    }
+  }
+
+  private updateCharacterMapFromInputVal(s: p5, inputVal: string): void {
+    const cleanedInputVal = inputVal.replace(/(\r\n|\n|\r|\s)/gm, '');
+    const inputValArr = cleanedInputVal.split('');
+    for (const char of inputValArr) {
+      const currCharCode = char.toLowerCase().charCodeAt(0);
+      if (!this.chars.has(currCharCode)) {
+        const maxLines = s.int(s.random(3, 6));
+        const padding = 10;
+        const charSize = 100;
+        this.chars.set(currCharCode, this.createTouchingLines(s, maxLines, padding, charSize));
       }
     }
   }
 
-  private generateHorizontalBarArr(s: p5, n: number) {
-    const result = [];
-    for (let i = 0; i < n; i++) {
-      const currX = s.int(s.random(0, 80));
-      result.push({
-        x: currX,
-        y: s.int(s.random(0, 90)),
-        w: s.int(s.random(10, 100 - currX)),
-        h: 5,
-      });
-    }
-    return result;
+  public handleInputTextChange(inputVal: string): void {
+    this.updateCharacterMapFromInputVal(this.canvas, inputVal);
   }
-
-  private generateVerticalBarArr(s: p5, n: number): any[] {
-    const result = [];
-    for (let i = 0; i < n; i++) {
-      const currY = s.int(s.random(0, 80));
-      result.push({
-        x: s.int(s.random(0, 90)),
-        y: currY,
-        w: 5,
-        h: s.int(s.random(10, 100 - currY)),
-      });
-    }
-    return result;
-  }
-
-  private generateLetter(s: p5): {
-    horizontalBarArr: any[];
-    verticalBarArr: any[];
-    dotTriplet: DotTriplet;
-  } {
-    // New shape-data generation:
-    const horizontalBarCount = s.random(0, 5);
-    const dotTriplet = new DotTriplet(
-      s.int(s.random(10, 91)),
-      s.int(s.random(10, 91))
-    );
-    const verticalBarCount = s.random(0, 5);
-    return {
-      horizontalBarArr: this.generateHorizontalBarArr(s, horizontalBarCount),
-      verticalBarArr: this.generateVerticalBarArr(s, verticalBarCount),
-      dotTriplet: dotTriplet,
-    };
-  }
-
-  private generateAlphabet(s: p5): void {
-    for (let char in this.chars) {
-      if (char === 'space' || char === 'return') {
-        //chars[char] = char;
-        continue;
-      }
-      const currLetter = this.generateLetter(s);
-      this.chars[char] = currLetter;
-    }
-  }
-
-  private translateInputStringIntoIoArr(s: p5, inputStr: string): any[] {
-    const strAsArr = inputStr.split('');
-    const outputArr = strAsArr.map((char) => {
-      let switchedChar = '';
-      switch (char) {
-        case '\n':
-          switchedChar = 'return';
-          break;
-        case ' ':
-          switchedChar = 'space';
-          break;
-        default:
-          switchedChar = char.toLowerCase();
-      }
-      return switchedChar;
-    });
-    return outputArr;
-  }
-
-  /* private generateDiagonalLineStartAndEnd(s: p5): {start: number[], end: number[]} {
-    // Generate random start point within canvas
-    const startX = s.random(10, 90);
-    const startY = s.random(10, 90);
-  
-    // Generate random line length between 10 and 100
-    const length = s.random(20, 80);
-  
-    // Calculate end point
-    const endX = startX + length;
-    const endY = Math.random() < 0.5 ? startY - length : startY + length;
-  
-    // Ensure end point is within canvas
-    if (endX > 90 || endY > 90 || endY < 10) {
-      return this.generateDiagonalLineStartAndEnd(s);
-    }
-  
-    return {
-      start: [startX, startY],
-      end: [endX, endY]
-    };
-  } */
 
   // ------ New Approach including diagonal lines with help of ChatGPT-4
 
@@ -379,7 +139,7 @@ export class TeA001Component implements OnInit, OnDestroy {
   private generateRandomLine(s: p5, padding: number, size: number) {
     let x1, y1, x2, y2!: number;
     switch (
-      s.int(s.random(3)) // 0: horizontal, 1: vertical, 2: diag down-right, 3: diag up-right
+      s.int(s.random(3)) // 0: horizontal, 1: vertical, 2: diagonal
     ) {
       case 0: // horizontal
         x1 = s.random(padding, size - padding);
@@ -391,7 +151,8 @@ export class TeA001Component implements OnInit, OnDestroy {
         y2 = s.random(y1, size - padding);
         x1 = x2 = s.random(padding, size - padding);
         break;
-      case 3: // diagona2
+      case 2: // diagona2
+        console.log('case 3');
         let length = s.random(5, size / 2 - padding);
         x1 = s.random(padding, size - padding - length);
         y1 = s.random(padding, size - padding - length);
@@ -482,24 +243,29 @@ export class TeA001Component implements OnInit, OnDestroy {
     return lines;
   }
 
+  private drawChar(s: p5, lineArr: any[]) {
+    lineArr.forEach((l) => {
+      s.strokeWeight(2);
+      s.stroke(237, 34, 93);
+      s.line(l.start.x, l.start.y, l.end.x, l.end.y);
+    });
+  }
+
   // p5.js setup function
-  setupMethod(s: p5) {
+  private setupMethod(s: p5) {
     s.createCanvas(100, 100);
     s.background(255);
     s.stroke(0);
 
     let padding = 10;
     let maxLines = 6;
-    let lines = this.createTouchingLines(s, maxLines, padding, 100);
+    let lines = this.createTouchingLines(s, maxLines, padding, 50);
 
-    lines.forEach((l) => {
-      s.strokeWeight(s.random(1, 3));
-      s.line(l.start.x, l.start.y, l.end.x, l.end.y);
-    });
+    this.drawChar(s, lines);
   }
 
   // p5.js draw function
-  drawMethod(s: p5) {
+  private drawMethod(s: p5) {
     // The drawing logic is handled in setup
     s.noLoop(); // No need to loop since the drawing only needs to happen once
   }
