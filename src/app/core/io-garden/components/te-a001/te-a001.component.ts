04116b6ed67dbf600efa00bab1d0fea5a4c87352
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-11-29 19:56:00 +0100

    refactor: cleanup TeA001Component and update description and meta data

--- src/app/core/io-garden/components/te-a001/te-a001.component.ts
+++ src/app/core/io-garden/components/te-a001/te-a001.component.ts
@@ -131,109 +131,6 @@ export class TeA001Component implements OnInit, OnDestroy {
 
   // ------ New Approach including diagonal lines with help of ChatGPT-4
 
-  private linesTouch(s: p5, l1: LineObj, l2: LineObj, touchThreshold = 1) {
-    function onSegment(p: {x: number, y: number}, q: {x: number, y: number}, r: {x: number, y: number}) {
-      if (q.x <= s.max(p.x, r.x) && q.x >= s.min(p.x, r.x) && q.y <= s.max(p.y, r.y) && q.y >= s.min(p.y, r.y)) {
-        return true;
-      }
-      return false;
-    }
-    
-    function orientation(p: {x: number, y: number}, q: {x: number, y: number}, r: {x: number, y: number}) {
-      let val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
-      if (val === 0) return 0; // colinear
-      return (val > 0) ? 1 : 2; // clock or counterclock wise
-    }
-    
-    function doIntersect(p1: {x: number, y: number}, q1: {x: number, y: number}, p2: {x: number, y: number}, q2: {x: number, y: number}) {
-      let o1 = orientation(p1, q1, p2);
-      let o2 = orientation(p1, q1, q2);
-      let o3 = orientation(p2, q2, p1);
-      let o4 = orientation(p2, q2, q1);
-      
-      if (o1 != o2 && o3 != o4) return true;
-      
-      if (o1 === 0 && onSegment(p1, p2, q1)) return true;
-      if (o2 === 0 && onSegment(p1, q2, q1)) return true;
-      if (o3 === 0 && onSegment(p2, p1, q2)) return true;
-      if (o4 === 0 && onSegment(p2, q1, q2)) return true;
-      
-      return false;
-    }
-    
-    // Check if line1 and line2 endpoints are touching without crossing
-    let endpoints = [l1.start, l1.end, l2.start, l2.end];
-    for (let i = 0; i < endpoints.length; i++) {
-      for (let j = 0; j < endpoints.length; j++) {
-        if (i !== j && endpoints[i].dist(endpoints[j]) <= touchThreshold && !doIntersect(l1.start, l1.end, endpoints[j], endpoints[(j + 2) % endpoints.length])) {
-          return true;
-        }
-      }
-    }
-    
-    return false;
-  }
-
-  // Function to check if two lines touch or cross
-  // This function returns true if the two lines intersect
-  private linesTouchOrCross(s: p5, l1: LineObj, l2: LineObj) {
-    // Convert points to vectors for easier calculation
-    let p1 = l1.start.copy();
-    let p2 = l1.end.copy();
-    let p3 = l2.start.copy();
-    let p4 = l2.end.copy();
-
-    // Get the vectors for the lines themselves
-    let line1 = p2.copy().sub(p1);
-    let line2 = p4.copy().sub(p3);
-
-    // Calculate denominators for the line equations
-    let denom = line1.x * line2.y - line1.y * line2.x;
-
-    // Lines are parallel if denominator is 0; here, we assume they don't overlap if parallel
-    if (denom === 0) return false;
-
-    // Calculate the numerator for the equations
-    let num1 = p1.x * p2.y - p1.y * p2.x;
-    let num2 = p3.x * p4.y - p3.y * p4.x;
-
-    // Intersection point
-    let ix = (num1 * line2.x - line1.x * num2) / denom;
-    let iy = (num1 * line2.y - line1.y * num2) / denom;
-
-    // Check if intersection point is within the line segments
-    let intersection = s.createVector(ix, iy);
-    if (
-      intersection.x < s.min(p1.x, p2.x) ||
-      intersection.x > s.max(p1.x, p2.x) ||
-      intersection.x < s.min(p3.x, p4.x) ||
-      intersection.x > s.max(p3.x, p4.x) ||
-      intersection.y < s.min(p1.y, p2.y) ||
-      intersection.y > s.max(p1.y, p2.y) ||
-      intersection.y < s.min(p3.y, p4.y) ||
-      intersection.y > s.max(p3.y, p4.y)
-    ) {
-      // Intersection point is outside the line segments
-      return false;
-    }
-
-    // If the intersection point is within both line segments, they cross
-    return true;
-  }
-
-  private createTouchingLines(s: p5, maxLines: number, padding: number, size: number) {
-    let lines = [];
-    while (lines.length < maxLines) {
-      let newLine = this.generateRandomLine(s, padding, size);
-      let touches = lines.some(l => this.linesTouch(s, l, newLine));
-      let crosses = lines.some(l => this.linesTouchOrCross(s, l, newLine));
-      if (touches && !crosses) {
-        lines.push(newLine);
-      }
-    }
-    return lines;
-  }
-
   private drawChar(s: p5, lineArr: LineObj[]) {
     lineArr.forEach((l) => {
       s.stroke(237, 34, 93);
@@ -243,26 +140,6 @@ export class TeA001Component implements OnInit, OnDestroy {
   }
 
   /// newest approach
-
-  private setupMethod(s: p5) {
-    s.createCanvas(100, 100);
-    s.background(255);
-    s.stroke(0);
-    s.strokeWeight(1);
-  
-    // Generate the starting line with extra padding
-    let startingLine = this.generateRandomLine(s, 15, 85); // 15% padding
-    this.allCurrLines = [startingLine];
-  
-    // First iteration: extend 1 to 2 lines from the starting line
-    this.extendLinesFromLine(s, startingLine, this.allCurrLines, 2);
-  
-    // Draw all lines
-    this.allCurrLines.forEach(l => {
-      s.strokeWeight(l.strokeWidth);
-      s.line(l.start.x, l.start.y, l.end.x, l.end.y);
-    });
-  }
   
   private generateRandomLine(s: p5, padding: number, size: number): LineObj {
     let x1, y1, x2, y2, orientation, strokeWidth;
@@ -391,9 +268,4 @@ export class TeA001Component implements OnInit, OnDestroy {
       }
     }
   }
-  
-  // Ensure the draw function does not loop
-/*   private drawMethod(s: p5) {
-    s.noLoop();
-  } */
 }
