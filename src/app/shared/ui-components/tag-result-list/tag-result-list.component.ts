064463ce6603fd175493f8c74be54640720a5249
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-12-10 19:20:52 +0100

    refactor: refactor TagResultListComponent

--- src/app/shared/ui-components/tag-result-list/tag-result-list.component.ts
+++ src/app/shared/ui-components/tag-result-list/tag-result-list.component.ts
@@ -19,22 +19,28 @@ export class TagResultListComponent implements OnChanges {
   @Input() tagResultList: (BlogPostMetaData | IoGardenExperimentMetaData)[] = [];
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('changes: ', changes)
     if (changes['tagResultList'].currentValue.length > 0) {
-      const isMobile = window.innerWidth < 768;
-      const rowHeight = isMobile ? 200 : 230;
-      const rowCount = isMobile ? this.tagResultList.length : Math.ceil(this.tagResultList.length / 2);
-      document.documentElement.style.setProperty('--row-count', `${rowCount}` );
-
-      const tagResultContainerHeight = rowCount * rowHeight + 2 + ((rowCount - 1) * 10)
-      document.documentElement.style.setProperty('--tag-result-container-height', `${tagResultContainerHeight}px`);
-
-      console.log('rowCount from variable', document.documentElement.style.getPropertyValue("--row-count"));
-      console.log('--tag-result-container-height', document.documentElement.style.getPropertyValue("--tag-result-container-height"))
+      this.calculateAndSetTagResultContainerHeight();
     } else {
       // document.documentElement.style.setProperty("--row-count", `0` );
       //console.log('rowCount from variable', document.documentElement.style.getPropertyValue("--row-count"));
-
     }
+  }
+
+  /**
+   * Calculates and sets the height of the tag result container CSS variable based on the number of rows
+   * and the display type. If the display is mobile, the row count is the length of the array.
+   * Otherwise, it is half the length of the array rounded up to the nearest integer.
+   * @returns void
+   */
+  private calculateAndSetTagResultContainerHeight(): void {
+    const isMobile = window.innerWidth < 768;
+    const rowHeight = isMobile ? 200 : 230;
+    // if the display is mobile, then the row count is the length of the array, 
+    // otherwise it is half the length of the array rounded up because there are two columns and we get half the number of rows 
+    // but we need to round up to get the correct number of rows for odd lengths.
+    const rowCount = isMobile ? this.tagResultList.length : Math.ceil(this.tagResultList.length / 2);
+    const tagResultContainerHeight = rowCount * rowHeight + 2 + ((rowCount - 1) * 10)
+    document.documentElement.style.setProperty('--tag-result-container-height', `${tagResultContainerHeight}px`);
   }
 }
