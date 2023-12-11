3d3c423abb8fdbf2d52aa91dfd671b7a81f68446
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-12-11 22:43:59 +0100

    misc: work on a css animation for fading out the search results when shortly before closing the container but no working solution found yet - will come back to it later

--- src/app/shared/ui-components/tag-result-list/tag-result-list.component.ts
+++ src/app/shared/ui-components/tag-result-list/tag-result-list.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { BlogPostMetaData } from '../../services/blog-service/blog.service';
 import { IoGardenExperimentMetaData } from '../../services/io-garden-service/io-garden.service';
 import { PreviewCardComponent } from '../preview-card/preview-card.component';
 import { NgClass } from '@angular/common';
+import { take, tap, timer } from 'rxjs';
 
 @Component({
   selector: 'app-tag-result-list',
@@ -17,13 +18,17 @@ import { NgClass } from '@angular/common';
 export class TagResultListComponent implements OnChanges {
 
   @Input() tagResultList: (BlogPostMetaData | IoGardenExperimentMetaData)[] = [];
+  public tagResultListSelectivelyDelayed: (BlogPostMetaData | IoGardenExperimentMetaData)[] = [];
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['tagResultList'].currentValue.length > 0) {
       this.calculateAndSetTagResultContainerHeight();
+      this.tagResultListSelectivelyDelayed = this.tagResultList;
     } else {
-      // document.documentElement.style.setProperty("--row-count", `0` );
-      //console.log('rowCount from variable', document.documentElement.style.getPropertyValue("--row-count"));
+      timer(300).pipe(
+        take(1),
+        tap(() => this.tagResultListSelectivelyDelayed = this.tagResultList)
+      ).subscribe();
     }
   }
 
@@ -34,13 +39,13 @@ export class TagResultListComponent implements OnChanges {
    * @returns void
    */
   private calculateAndSetTagResultContainerHeight(): void {
-    const isMobile = window.innerWidth < 768;
+    const isMobile = window.innerWidth <= 768;
     const rowHeight = isMobile ? 200 : 230;
     // if the display is mobile, then the row count is the length of the array, 
     // otherwise it is half the length of the array rounded up because there are two columns and we get half the number of rows 
     // but we need to round up to get the correct number of rows for odd lengths.
     const rowCount = isMobile ? this.tagResultList.length : Math.ceil(this.tagResultList.length / 2);
-    const tagResultContainerHeight = rowCount * rowHeight + 2 + ((rowCount - 1) * 10)
+    const tagResultContainerHeight = rowCount * rowHeight + 4 + ((rowCount - 1) * 10)
     document.documentElement.style.setProperty('--tag-result-container-height', `${tagResultContainerHeight}px`);
   }
 }
