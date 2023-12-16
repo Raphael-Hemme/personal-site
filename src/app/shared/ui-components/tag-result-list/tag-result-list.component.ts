6072a9cdc3d28005ed0e230d30608d5a53838631
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-12-16 12:23:15 +0100

    refactor: rework the animation of the enttries in the TagResultListComponent

--- src/app/shared/ui-components/tag-result-list/tag-result-list.component.ts
+++ src/app/shared/ui-components/tag-result-list/tag-result-list.component.ts
@@ -4,48 +4,53 @@ import { IoGardenExperimentMetaData } from '../../services/io-garden-service/io-
 import { PreviewCardComponent } from '../preview-card/preview-card.component';
 import { NgClass } from '@angular/common';
 import { take, tap, timer } from 'rxjs';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-tag-result-list',
+  templateUrl: './tag-result-list.component.html',
+  styleUrl: './tag-result-list.component.scss',
   standalone: true,
   imports: [ 
     NgClass,
     PreviewCardComponent 
   ],
-  templateUrl: './tag-result-list.component.html',
-  styleUrl: './tag-result-list.component.scss'
+  animations: [
+    trigger('fadeInOut', [
+      state('enter', style({ 
+        opacity: 0,
+      })),
+      state('leave', style({ 
+        opacity: 0,
+      })),
+      transition('enter => leave', [
+        animate(300)
+      ]),
+      transition('leave => enter', [
+        animate(300)
+      ])
+    ])
+  ]
 })
 export class TagResultListComponent implements OnChanges {
 
   @Input() tagResultList: (BlogPostMetaData | IoGardenExperimentMetaData)[] = [];
-  // public tagResultListSelectivelyDelayed: (BlogPostMetaData | IoGardenExperimentMetaData)[] = [];
+  public tagResultListSelectivelyDelayed: (BlogPostMetaData | IoGardenExperimentMetaData)[] = [];
+  public tagResultEntryIsVisible = false;
 
   ngOnChanges(changes: SimpleChanges): void {
-    /* if (changes['tagResultList'].currentValue.length > 0) {
-      this.calculateAndSetTagResultContainerHeight();
+    if (changes['tagResultList'].currentValue.length > 0) {
+      console.log('tagResultList filled -- setting tagResultEntryIsVisible to true');
+
+      this.tagResultEntryIsVisible = true;
       this.tagResultListSelectivelyDelayed = this.tagResultList;
     } else {
-      timer(300).pipe(
+      console.log('tagResultList is empty -- setting tagResultEntryIsVisible to false');
+      this.tagResultEntryIsVisible = false;
+      timer(500).pipe(
         take(1),
         tap(() => this.tagResultListSelectivelyDelayed = this.tagResultList)
       ).subscribe();
-    } */
+    }
   }
-
-  /**
-   * Calculates and sets the height of the tag result container CSS variable based on the number of rows
-   * and the display type. If the display is mobile, the row count is the length of the array.
-   * Otherwise, it is half the length of the array rounded up to the nearest integer.
-   * @returns void
-   */
-  /* private calculateAndSetTagResultContainerHeight(): void {
-    const isMobile = window.innerWidth <= 768;
-    const rowHeight = isMobile ? 200 : 230;
-    // if the display is mobile, then the row count is the length of the array, 
-    // otherwise it is half the length of the array rounded up because there are two columns and we get half the number of rows 
-    // but we need to round up to get the correct number of rows for odd lengths.
-    const rowCount = isMobile ? this.tagResultList.length : Math.ceil(this.tagResultList.length / 2);
-    const tagResultContainerHeight = rowCount * rowHeight + 4 + ((rowCount - 1) * 10)
-    document.documentElement.style.setProperty('--tag-result-container-height', `${tagResultContainerHeight}px`);
-  } */
 }
