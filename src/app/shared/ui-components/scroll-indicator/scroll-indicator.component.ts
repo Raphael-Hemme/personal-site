b4cc6edd061b31d71e6221f9116f4fc2f381f1df
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-12-28 13:46:31 +0100

    feat: Add complex rxjs-css animation to the scroll indicator

--- src/app/shared/ui-components/scroll-indicator/scroll-indicator.component.ts
+++ src/app/shared/ui-components/scroll-indicator/scroll-indicator.component.ts
@@ -1,5 +1,6 @@
-import { NgClass } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { AsyncPipe, NgClass } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, Subscription, interval, tap } from 'rxjs';
 
 interface DotMatrixEntry {
   isActive: boolean;
@@ -9,11 +10,14 @@ interface DotMatrixEntry {
 @Component({
   selector: 'app-scroll-indicator',
   standalone: true,
-  imports: [NgClass],
+  imports: [
+    NgClass,
+    AsyncPipe
+  ],
   templateUrl: './scroll-indicator.component.html',
   styleUrl: './scroll-indicator.component.scss'
 })
-export class ScrollIndicatorComponent implements OnInit {
+export class ScrollIndicatorComponent implements OnInit, OnDestroy {
 
   public height = 230;
   public width = 20;
@@ -24,12 +28,21 @@ export class ScrollIndicatorComponent implements OnInit {
 
   public dotMatrix: DotMatrixEntry[] = [];
 
+  private dotMatrix$$ = new BehaviorSubject<DotMatrixEntry[]>([]);
+  public dotMatrix$ = this.dotMatrix$$.asObservable();
+
   private rowHeight = 5;
   private columnNum = 4;
   private rowNum = Math.floor(this.height / this.rowHeight);
 
+  private subscriptions = new Subscription();
+
   ngOnInit(): void {
-    this.dotMatrix = this.generateDotMatrix(this.columnNum, this.rowNum);
+    this.dotMatrix = this.generateInactiveDotMatrix(this.columnNum, this.rowNum);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
   public updateIndicatorFromProps(props: { left: number, top: number, height: number }): void {
@@ -37,21 +50,58 @@ export class ScrollIndicatorComponent implements OnInit {
     this.top = props.top;
     this.height = props.height;
     this.rowNum = Math.floor(this.height / this.rowHeight);
-    this.dotMatrix = this.generateDotMatrix(this.columnNum, this.rowNum);
+    // this.dotMatrix = this.generateInactiveDotMatrix(this.columnNum, this.rowNum);
+    this.startAnimation();
   }
 
-  private generateDotMatrix(colNum: number, rowNum: number): DotMatrixEntry[] {
+  private generateInactiveDotMatrix(colNum: number, rowNum: number): DotMatrixEntry[] {
     const matrix = [];
     for (let i = 0; i < rowNum; i++) {
       const row = [];
       for (let j = 0; j < colNum; j++) {
         row.push({
-          isActive: Math.random() > 0.7 ? true : false,
+          isActive: false,
           id: `${i}-${j}`
         });
       }
       matrix.push(row);
     }
     return matrix.flat();
+  }
+
+  private startAnimation(): void {
+    this.subscriptions.add(
+      interval(35).pipe(
+        tap((intervalValue) => {
+          const rowIterator = this.getRowIteratorFromIntervalValue(intervalValue);
+          this.dotMatrix$$.next(this.updateDotMatrixInAnimationLoop(rowIterator));
+        })
+      ).subscribe()
+    )
+  }
+
+  private updateDotMatrixInAnimationLoop(rowIterator: number): DotMatrixEntry[] {
+    return this.generateInactiveDotMatrix(this.columnNum, this.rowNum)
+      .map((dotMatrixEntry) => {
+        const rowIdentifier = dotMatrixEntry.id.split('-')[0];
+        if (rowIdentifier === rowIterator.toString()) {
+          return {
+            ...dotMatrixEntry,
+            isActive: Math.random() > 0.8
+          }
+        } else {
+          return dotMatrixEntry
+        }
+      })
+  }
+
+  private getRowIteratorFromIntervalValue(intervalValue: number): number {
+    let result = 0;
+    if (intervalValue >= this.rowNum) {
+      result = intervalValue % this.rowNum - 1;
+    } else {
+      result = intervalValue;
+    }
+    return result;
   }
 }
