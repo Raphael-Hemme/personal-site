e9b877e44cf06618d3fd56384b365723b75f0a81
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-12-29 13:29:47 +0100

    refactor: Refactor scroll indicator positioning for mobile and desktop

--- src/app/shared/ui-components/scroll-indicator/scroll-indicator.directive.ts
+++ src/app/shared/ui-components/scroll-indicator/scroll-indicator.directive.ts
@@ -19,6 +19,8 @@ export class ScrollIndicatorDirective implements OnChanges, OnDestroy {
   private tagSelectionListPrevState = false;
 
   private windowResize$: Observable<any> = fromEvent(window, 'resize');
+  private isMobile = this.breakpointObserver.isMatched('(max-width: 768px)');
+
   private resizeObserver = new ResizeObserver(entries => {
     this.setScrollIndicatorComponentProperties();
   });
@@ -32,14 +34,19 @@ export class ScrollIndicatorDirective implements OnChanges, OnDestroy {
   ) { 
     this.subscriptions.add(
       this.windowResize$.pipe(
-        tap(() => this.createOrUpdateScrollIndicatorInstance())
+        tap(() => {
+          this.isMobile = this.breakpointObserver.isMatched('(max-width: 768px)');
+          this.createOrUpdateScrollIndicatorInstance()
+        })
       ).subscribe()
     )
     this.resizeObserver.observe(document.body);
   }
 
   ngOnChanges(changes: SimpleChanges): void  {
-    if ('scrollIndicator' in changes && changes['scrollIndicator']?.currentValue?.tagResultListIsVisible) {
+    if ('scrollIndicator' in changes 
+      && changes['scrollIndicator']?.currentValue?.tagResultListIsVisible
+    ) {
       this.createOrUpdateScrollIndicatorInstance();
     } else {
       this.destroy();
@@ -74,9 +81,20 @@ export class ScrollIndicatorDirective implements OnChanges, OnDestroy {
         take(1),
         tap(() => {
           const { left, top, bottom } = this.elementRef.nativeElement.getBoundingClientRect();
+
+          // On mobile, the scroll indicator should be placed over the preview image of the card.
+          // On every width bigger than mobile, the scroll indicator should be placed to the left of the conainer.
+          const leftPos = this.isMobile 
+            ? left + 5 // mobile
+            : left - this.compWidth - 5; // everything up from mobile
+
+          // Calculate the top position by adding 5px to the top to account for the border radius of the card 
+          // and add scrollY to account for the current scroll-position
+          const topPos = top + 5 + window.scrollY 
+
           this.componentRef!.instance.updateIndicatorFromProps({
-            left: left - this.compWidth - 5,
-            top: top + 2 + window.scrollY,
+            left: leftPos,
+            top: topPos,
             height: bottom - top
           });
         })
@@ -87,8 +105,8 @@ export class ScrollIndicatorDirective implements OnChanges, OnDestroy {
 
   private destroy(): void {
     if (this.componentRef !== null) {
-      this.viewRef.detach();
-      this.viewRef.remove();
+      // this.viewRef.detach();
+      // this.viewRef.remove();
       this.viewRef.clear();
       this.componentRef.destroy();
       this.componentRef = null;
